208a8e6d7410bb165ee7ad1b2a2f7edebd61ca80
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-19 20:16:02 -0800

    finished class

---
+++ src/class.js
@@ -0,0 +1,60 @@
+'use strict';
+
+class Tree {
+  constructor(){
+    this.fruit = 0;
+  }
+  makeFruits(){
+    ++this.fruit;
+  }
+}
+
+function main(numPpl, numFruits) {
+  var plants = [];
+  var week = 1;
+  var result = 0;
+  calculate(numPpl, numFruits);
+  function calculate(numPpl, numFruits){
+    if (week ===1) {
+      for (var ii = 0; ii < numFruits; ii++) {
+        plants.push(new Tree);
+        // console.log(plants);
+      }
+      week++;
+    }//end of if
+    else {
+      for (var jj = 0; jj < numFruits; jj++){
+        plants.push(new Tree());
+      }
+      week++;
+    }//end of else
+
+    var totalFruits = 0;
+    // console.log('HERE',totalFruits);
+
+    plants.forEach( plant => {
+      plant.makeFruits();
+      totalFruits += plant.fruit;
+    });
+
+    // console.log('TOTAL FRUITS', totalFruits);
+
+    if (totalFruits < numPpl) {
+      calculate(numPpl, totalFruits);
+    }
+    else {
+      result = week;
+    }
+  }//end of function calculate
+  return result;
+}
+
+// var a = main(200, 15);
+// console.log(a);
+// var b = main(15, 1);
+// console.log(b);
+// var c = main(50000, 1);
+// console.log(c);
+// var d = main(150000, 250);
+// console.log(d);
+

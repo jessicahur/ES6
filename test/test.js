f01d50c077fdf04e4634f091f2374bd04ccff0cf
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-21 11:00:33 -0800

    make sure test.js uses arow functions

--- test/test.js
+++ test/test.js
@@ -5,9 +5,9 @@ const expect = chai.expect;
 
 import main from '../src/class';
 
-describe ('CLASS ES6', function() {
+describe ('CLASS ES6', () => {
 
-  it('should sucessfully import the export default function and give the expected answer', function(){
+  it('should sucessfully import the export default function and give the expected answer', () => {
     let array = [[], [200,15], [50000,1], [150000, 250]];
     let results = [5, 5, 14, 9];
     let calculated = array.map((elemArray) => main(elemArray[0], elemArray[1]));
